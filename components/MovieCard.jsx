c0fc509e36e9d2cebd70104fda708cef6ef9afaf
Fabricio <marcofabri6@gmail.com>
2023-07-02 21:11:54 -0600

    fix: add some changes to the movie card layout

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -1,29 +1,35 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 const MovieCard = () => {
   return (
-    <div className="text-white w-fit">
-      <Image
-        src="/assets/barbie.jpg"
-        width={300}
-        height={300}
-        alt="movie poster"
-        className="rounded-md"
-      ></Image>
-      <div>
-        <div className="flex justify-between">
-          <h1>Barbie</h1>
-          <p>2023</p>
-        </div>
-        <div className="flex justify-between">
-          <p>4K</p>
-          <div className="flex justify-between gap-5">
-            <p>90 min</p>
-            <p>4.5</p>
+    <div className="p-2 text-white rounded-md w-fit">
+      <motion.div whileHover={{ scale: 1.05 }}>
+        <Image
+          src="/assets/barbie.jpg"
+          width={300}
+          height={300}
+          alt="movie poster"
+          className="rounded-md"
+        ></Image>
+
+        <div className="mt-5">
+          <div className="flex items-center justify-between">
+            <h1 className="text-4xl">Barbie</h1>
+            <p>2023</p>
+          </div>
+          <div className="flex mt-5 justify-between">
+            <p>4K</p>
+            <div className="flex justify-between gap-5">
+              <p>90 min</p>
+              <p>4.5</p>
+            </div>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
