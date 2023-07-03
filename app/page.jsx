f1009a2fab75f04bf9d2bbdcb413c2dda6107c95
Fabricio <marcofabri6@gmail.com>
2023-07-03 11:48:00 -0600

    feat: create animation on hero section when page loads

--- app/page.jsx
+++ app/page.jsx
@@ -1,40 +1,11 @@
 import React from "react";
 import MovieCard from "@components/MovieCard";
-import Image from "next/image";
-import { AiFillPlayCircle } from "react-icons/ai";
-import MainBtn from "@components/ui/MainBtn";
+import HeroSection from "@components/HeroSection";
+
 const Home = () => {
   return (
     <>
-      <div>
-        <div class="relative">
-          <Image
-            src="/assets/test1.jpg"
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{
-              width: "100%",
-              height: "800px",
-              objectFit: "cover",
-              opacity: "0.25",
-            }} // optional
-            alt="main image"
-          ></Image>
-          <div className="absolute top-1/2 left-1/4 md:-translate-x-80 -translate-y-1/2">
-            <h1 className="text-white text-5xl font-extrabold main-color">
-              MovieHub
-            </h1>
-            <p className="text-4xl text-white max-w-md mt-10">
-              Track Your <span className="main-color"> Favorite</span> Movies &
-              TV Shows
-            </p>
-            <div className="mt-10">
-              <MainBtn icon={<AiFillPlayCircle />} text="Watch Now" url="/" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <HeroSection />
       <div className="text-white px-5 mt-16">
         <h1 className="text-4xl text-center md:text-left">Upcoming Movies</h1>
       </div>
---
+++ components/HeroSection.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { AiFillPlayCircle } from "react-icons/ai";
+import MainBtn from "@components/ui/MainBtn";
+
+const HeroSection = () => {
+  return (
+    <div class="relative">
+      <Image
+        src="/assets/test1.jpg"
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{
+          width: "100%",
+          height: "800px",
+          objectFit: "cover",
+          opacity: "0.25",
+        }} // optional
+        alt="main image"
+      ></Image>
+      <motion.div
+        initial={{ y: 400, x: -250 }}
+        animate={{ y: -150 }}
+        transition={{ duration: 1 }}
+        className="absolute top-1/2 left-1/4 md:-translate-x-80 -translate-y-1/2"
+      >
+        <h1 className="text-white text-5xl font-extrabold main-color">
+          MovieHub
+        </h1>
+        <p className="text-4xl text-white max-w-md mt-10">
+          Track Your <span className="main-color"> Favorite</span> Movies & TV
+          Shows
+        </p>
+        <div className="mt-10">
+          <MainBtn icon={<AiFillPlayCircle />} text="Watch Now" url="/" />
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default HeroSection;
