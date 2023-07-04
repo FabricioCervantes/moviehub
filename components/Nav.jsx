3787bfb58f3068c72c51fadafed08844dbdf1764
Fabricio <marcofabri6@gmail.com>
2023-07-04 14:18:38 -0600

    fix: hamburger menu is not working while the navbar is sticky

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -15,7 +15,7 @@ import {
 
 const Nav = () => {
   return (
-    <nav className="flex sticky top-0 bg-transparent justify-between text-white p-5 items-center">
+    <nav className="flex bg-transparent justify-between text-white p-5 items-center">
       <Link href="/">
         <Image
           src="/assets/logo.png"
