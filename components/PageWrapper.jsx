29445d53b0fb6a306b2400dc6e398ad378608764
Fabricio <marcofabri6@gmail.com>
2023-07-03 20:25:37 -0600

    fix: opacity in PageWrapper was not being changed to 1

--- components/PageWrapper.jsx
+++ components/PageWrapper.jsx
@@ -6,7 +6,7 @@ export const PageWrapper = ({ children }) => (
     <motion.div
       initial={{ opacity: 0, y: 15 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{ delay: 0.25 }}
+      // transition={{ delay: 0.25 }}
     >
       {children}
     </motion.div>
