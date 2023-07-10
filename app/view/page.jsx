a357833454b0a494cfa4eb618824445656c1efa2
Fabricio <marcofabri6@gmail.com>
2023-07-09 20:10:06 -0600

    fix: animation

--- app/view/page.jsx
+++ app/view/page.jsx
@@ -2,9 +2,8 @@ import MovieInfo from "@components/MovieInfo";
 
 const View = () => {
   return (
-    <div className="text-3xl text-white">
-      fuck
-      {/* <MovieInfo /> */}
+    <div>
+      <MovieInfo />
     </div>
   );
 };
