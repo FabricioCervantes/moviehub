1a5f6fdc2db897587beec4d9e229f73f4b5ca885
Fabricio <marcofabri6@gmail.com>
2023-07-21 10:47:52 -0600

    fix: search bar width on mobile

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,6 +9,8 @@ import MainBtn from "./ui/MainBtn";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import SearchBar from "./SearchBar";
+import { BiSearch } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
 import {
   DropdownMenu,
@@ -24,6 +26,7 @@ const Nav = () => {
 
   const [providers, setProviders] = useState(null);
   const [toggleDropdown, setToggleDropdown] = useState(false);
+  const [toggleSearch, setToggleSearch] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -34,113 +37,142 @@ const Nav = () => {
 
   return (
     <nav className="flex bg-transparent justify-between text-white p-5 items-center">
-      <Link href="/">
-        <Image
-          src="/assets/logo.png"
-          alt="logo"
-          height={150}
-          width={150}
-        ></Image>
-      </Link>
-      <SearchBar />
-      <ul className="hidden md:flex gap-20">
-        <Link href="/movies">
-          <div className="text-2xl font-bold">MOVIES</div>
+      {!toggleSearch && (
+        <Link href="/">
+          <Image
+            src="/assets/logo.png"
+            alt="logo"
+            height={150}
+            width={150}
+          ></Image>
         </Link>
-        <Link href="/tv">
-          <div className="text-2xl font-bold">TV SHOWS</div>
-        </Link>
-        {/* <Link href="/">
+      )}
+      <div className="flex gap-2">
+        <div className="hidden md:block">
+          <SearchBar />
+        </div>
+        <ul className="hidden md:flex gap-20">
+          <Link href="/movies">
+            <div className="text-2xl font-bold">MOVIES</div>
+          </Link>
+          <Link href="/tv">
+            <div className="text-2xl font-bold">TV SHOWS</div>
+          </Link>
+          {/* <Link href="/">
           <div className="text-2xl font-bold">PRICING</div>
         </Link>
         <Link href="/">
           <div className="text-2xl font-bold">ABOUT</div>
         </Link> */}
-      </ul>
-      {session?.user ? (
-        <div className="flex gap-5 items-center">
-          <ul className="hidden md:block">
-            <MainBtn action={signOut} text="Sign Out" />
-          </ul>
-          <Avatar className="hidden md:block">
-            <AvatarImage src={session?.user.image} />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-        </div>
-      ) : (
-        <ul className="hidden md:block">
-          {providers &&
-            Object.values(providers).map((provider) => (
-              <MainBtn
-                type="button"
-                key={provider.name}
-                action={() => {
-                  signIn(provider.id);
-                }}
-                text="Sign In"
-              ></MainBtn>
-            ))}
         </ul>
-      )}
-      <ul className="md:hidden">
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <GiHamburgerMenu className="text-4xl" />
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="bg border-0 text-white w-screen h-screen flex flex-col pt-20 gap-8 items-center">
-            <DropdownMenuLabel>
-              {session?.user ? (
-                <div className="flex flex-col gap-10 items-center">
-                  <Avatar className="h-32 w-auto">
-                    <AvatarImage src={session?.user.image} />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                  <ul>
-                    <MainBtn action={signOut} text="Sign Out" />
-                  </ul>
-                </div>
-              ) : (
-                <ul>
-                  {providers &&
-                    Object.values(providers).map((provider) => (
-                      <MainBtn
-                        type="button"
-                        key={provider.name}
-                        action={() => {
-                          signIn(provider.id);
-                        }}
-                        text="Sign In"
-                      ></MainBtn>
-                    ))}
-                </ul>
-              )}
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-              <Link href="/movies">
-                <div className="text-4xl">MOVIES</div>
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              <Link href="/tv">
-                <div className="text-4xl">TV SHOWS</div>
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              {" "}
-              <Link href="/">
-                <div className="text-4xl">PRICING</div>
-              </Link>
-            </DropdownMenuItem>
-            <DropdownMenuItem>
-              {" "}
-              <Link href="/">
-                <div className="text-4xl">ABOUT</div>
-              </Link>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </ul>
+        {session?.user ? (
+          <div className="flex gap-5 items-center">
+            <ul className="hidden md:block">
+              <MainBtn action={signOut} text="Sign Out" />
+            </ul>
+            <Avatar className="hidden md:block">
+              <AvatarImage src={session?.user.image} />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+        ) : (
+          <ul className="hidden md:block">
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <MainBtn
+                  type="button"
+                  key={provider.name}
+                  action={() => {
+                    signIn(provider.id);
+                  }}
+                  text="Sign In"
+                ></MainBtn>
+              ))}
+          </ul>
+        )}
+        <div className="md:hidden text-4xl">
+          {!toggleSearch && (
+            <BiSearch
+              onClick={() => {
+                setToggleSearch(!toggleSearch);
+              }}
+            />
+          )}
+
+          {toggleSearch && (
+            <div className="flex items-center w-full gap-2">
+              <AiOutlineClose
+                onClick={() => {
+                  setToggleSearch(!toggleSearch);
+                }}
+                className="text-2xl"
+              />
+              <SearchBar />
+            </div>
+          )}
+        </div>
+        <ul className="md:hidden">
+          {!toggleSearch && (
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <GiHamburgerMenu className="text-4xl" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="bg border-0 text-white w-screen h-screen flex flex-col pt-20 gap-8 items-center">
+                <DropdownMenuLabel>
+                  {session?.user ? (
+                    <div className="flex flex-col gap-10 items-center">
+                      <Avatar className="h-32 w-auto">
+                        <AvatarImage src={session?.user.image} />
+                        <AvatarFallback>CN</AvatarFallback>
+                      </Avatar>
+                      <ul>
+                        <MainBtn action={signOut} text="Sign Out" />
+                      </ul>
+                    </div>
+                  ) : (
+                    <ul>
+                      {providers &&
+                        Object.values(providers).map((provider) => (
+                          <MainBtn
+                            type="button"
+                            key={provider.name}
+                            action={() => {
+                              signIn(provider.id);
+                            }}
+                            text="Sign In"
+                          ></MainBtn>
+                        ))}
+                    </ul>
+                  )}
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem asChild>
+                  <Link href="/movies">
+                    <div className="text-4xl">MOVIES</div>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link href="/tv">
+                    <div className="text-4xl">TV SHOWS</div>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  {" "}
+                  <Link href="/">
+                    <div className="text-4xl">PRICING</div>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  {" "}
+                  <Link href="/">
+                    <div className="text-4xl">ABOUT</div>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </ul>
+      </div>
     </nav>
   );
 };
--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -86,8 +86,8 @@ const SearchBar = () => {
   };
 
   return (
-    <div className="w-[600px]" ref={searchContainerRef}>
-      <div className="flex text-black bg-white p-2 rounded-md">
+    <div className="w-[300px] md:w-[600px]" ref={searchContainerRef}>
+      <div className="flex text-white nav-bg p-2 rounded-md w-full ">
         <input
           type="text"
           placeholder="Search movie..."
@@ -101,16 +101,9 @@ const SearchBar = () => {
               clearInput();
             }
           }}
-          clear
-          search
-          bar
-          when
-          user
-          press
-          esc
-          className="focus:outline-none w-full"
+          className="focus:outline-none md:w-full bg-transparent text-sm md:text-xl"
         />
-        <div className="text-black text-2xl">
+        <div className="text-white text-2xl hidden md:block">
           {filteredData.length === 0 ? (
             <BiSearch />
           ) : (
@@ -119,7 +112,7 @@ const SearchBar = () => {
         </div>
       </div>
       {showResults && filteredData.length !== 0 && (
-        <div className="bg-white text-black rounded-md mt-2 absolute w-[600px] p-2">
+        <div className="text-white rounded-md mt-2 nav-bg absolute w-[300px] p-2">
           {filteredData.slice(0, 4).map((value, key) => {
             return (
               <div
