960755ff85d3cb8fb5cc17bab943a14111484509
Fabricio <marcofabri6@gmail.com>
2023-07-21 17:39:24 -0600

    improve loading speed of movies page

--- app/movies/page.jsx
+++ app/movies/page.jsx
@@ -1,4 +1,6 @@
+"use client";
 import DisplayMedia from "@components/DisplayMedia";
+import { useEffect, useState } from "react";
 import {
   Select,
   SelectContent,
@@ -7,45 +9,40 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 
-const Movies = async () => {
-  // const [movies, setMovies] = useState([]);
-  // const [genre, setGenre] = useState([]);
-  // const [selectedGenre, setSelectedGenre] = useState(28);
-  // const [page, setPage] = useState(1);
-  // const [sort, setSort] = useState("popularity.desc");
-
-  const fetchUpcomingMovies = () => {
-    return fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US`
-    ).then((res) => res.json());
-  };
-
-  let movies = await fetchUpcomingMovies();
-
-  // let genre = 28;
-  // let genreList = await fetchGenre();
+const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  const [genre, setGenre] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState(28);
+  const [page, setPage] = useState(1);
+  const [sort, setSort] = useState("popularity.desc");
 
   const pag = Array.from(Array(5).keys()).map((i) => i + 1);
 
-  // const fetchGenre = () => {
-  //   return fetch(
-  //     `https://api.themoviedb.org/3/genre/movie/list?api_key=a97a0e69992c3fbbfda4f5387a476249`
-  //   ).then((res) => res.json());
-  // };
+  const fetchGenre = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=a97a0e69992c3fbbfda4f5387a476249`
+    ).then((res) => res.json());
+  };
 
-  // const fetchMovies = async () => {
-  //   const test = await fetchGenre();
-  //   const upcomingMovies = await fetchUpcomingMovies();
-  //   movies = upcomingMovies.results;
-  //   genreList = test.genres;
-  // };
+  const fetchUpcomingMovies = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=${page}&with_genres=${selectedGenre}&sort_by=${sort}`
+    ).then((res) => res.json());
+  };
+
+  const fetchMovies = async () => {
+    const test = await fetchGenre();
+    const upcomingMovies = await fetchUpcomingMovies();
+    setMovies(upcomingMovies.results);
+    setGenre(test.genres);
+  };
 
   const handlePage = (item) => {
     setPage(item);
     fetchMovies();
   };
   const handleGenre = (value) => {
-    genre = value;
+    setSelectedGenre(value);
     setPage(1);
     fetchMovies();
   };
@@ -56,26 +53,26 @@ const Movies = async () => {
     fetchMovies();
   };
 
-  // useEffect(() => {
-  //   fetchMovies();
-  // }, [sort, page, selectedGenre]);
+  useEffect(() => {
+    fetchMovies();
+  }, [sort, page, selectedGenre]);
 
   return (
     <>
       <div className="flex text-white px-5 justify-end gap-5">
-        {/* <Select onValueChange={(e) => handleGenre(e)}>
+        <Select onValueChange={(e) => handleGenre(e)}>
           <SelectTrigger className="w-[180px] border-2">
             <SelectValue placeholder="Genre" />
           </SelectTrigger>
           <SelectContent>
-            {genreList.map((item) => (
+            {genre.map((item) => (
               <SelectItem key={item.id} value={item.id}>
                 {item.name}
               </SelectItem>
             ))}
           </SelectContent>
-        </Select> */}
-        {/* <Select onValueChange={(e) => handleSort(e)}>
+        </Select>
+        <Select onValueChange={(e) => handleSort(e)}>
           <SelectTrigger className="w-[180px] border-2">
             <SelectValue placeholder="Sort by" />
           </SelectTrigger>
@@ -84,13 +81,10 @@ const Movies = async () => {
             <SelectItem value="vote_count.desc">Popular</SelectItem>
             <SelectItem value="revenue.desc">Box Office</SelectItem>
           </SelectContent>
-        </Select> */}
+        </Select>
       </div>
-      {movies && movies.results && (
-        <DisplayMedia media={movies.results} type="movie" />
-      )}
-
-      {/* <div className="flex justify-end gap-5 p-2 mt-5">
+      <DisplayMedia media={movies} type="movie" />
+      <div className="flex justify-end gap-5 p-2 mt-5">
         {pag.map((item) => (
           <p
             key={item}
@@ -109,7 +103,7 @@ const Movies = async () => {
             {item}
           </p>
         ))}
-      </div> */}
+      </div>
     </>
   );
 };
