8af6080dba85eab16938cd3af6ca6209eb2a4302
Fabricio <marcofabri6@gmail.com>
2023-07-28 19:57:19 -0600

    feat: card uses link instead of route.push for movie or tv page

--- app/tv/page.jsx
+++ app/tv/page.jsx
@@ -10,6 +10,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import LoadingAnimation from "@components/LoadingAnimation";
 
 const tv = () => {
   const [shows, setShows] = useState([]);
@@ -17,6 +18,7 @@ const tv = () => {
   const [selectedGenre, setSelectedGenre] = useState();
   const [page, setPage] = useState(1);
   const [sort, setSort] = useState("vote_count.desc");
+  const [loading, setLoading] = useState(true);
 
   const pag = Array.from(Array(5).keys()).map((i) => i + 1);
 
@@ -33,10 +35,12 @@ const tv = () => {
   };
 
   const fetchShows = async () => {
+    setLoading(true);
     const test = await fetchGenre();
     const upcomingMovies = await fetchUpcomingShows();
     setShows(upcomingMovies.results);
     setGenre(test.genres);
+    setLoading(false);
   };
 
   const handlePage = (item) => {
@@ -62,51 +66,58 @@ const tv = () => {
   console.log(shows);
   return (
     <>
-      <div className="flex text-white px-5 justify-end gap-5">
-        <Select onValueChange={(e) => handleGenre(e)}>
-          <SelectTrigger className="w-[180px] border-2">
-            <SelectValue placeholder="Genre" />
-          </SelectTrigger>
-          <SelectContent>
-            {genre.map((item) => (
-              <SelectItem key={item.id} value={item.id}>
-                {item.name}
-              </SelectItem>
+      {loading ? (
+        <LoadingAnimation />
+      ) : (
+        <>
+          {" "}
+          <div className="flex text-white px-5 justify-end gap-5">
+            <Select onValueChange={(e) => handleGenre(e)}>
+              <SelectTrigger className="w-[180px] border-2">
+                <SelectValue placeholder="Genre" />
+              </SelectTrigger>
+              <SelectContent>
+                {genre.map((item) => (
+                  <SelectItem key={item.id} value={item.id}>
+                    {item.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <Select onValueChange={(e) => handleSort(e)}>
+              <SelectTrigger className="w-[180px] border-2">
+                <SelectValue placeholder="Sort by" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="popularity.desc">Trending</SelectItem>
+                <SelectItem value="vote_count.desc">Popular</SelectItem>
+                <SelectItem value="revenue.desc">Box Office</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <DisplayMedia media={shows} type="shows" />
+          <div className="flex justify-end gap-5 p-2 mt-5">
+            {pag.map((item) => (
+              <p
+                key={item}
+                onClick={() => {
+                  handlePage(item);
+                  window.scrollTo({
+                    top: 0,
+                    behavior: "smooth",
+                  });
+                }}
+                //check if page is active
+                className={`${
+                  page === item ? "bg-red-500" : "bg-gray-500"
+                } px-5 py-2 rounded-md hover:cursor-pointer hover:bg-red-500 hover:text-white`}
+              >
+                {item}
+              </p>
             ))}
-          </SelectContent>
-        </Select>
-        <Select onValueChange={(e) => handleSort(e)}>
-          <SelectTrigger className="w-[180px] border-2">
-            <SelectValue placeholder="Sort by" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="popularity.desc">Trending</SelectItem>
-            <SelectItem value="vote_count.desc">Popular</SelectItem>
-            <SelectItem value="revenue.desc">Box Office</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-      <DisplayMedia media={shows} type="shows" />
-      <div className="flex justify-end gap-5 p-2 mt-5">
-        {pag.map((item) => (
-          <p
-            key={item}
-            onClick={() => {
-              handlePage(item);
-              window.scrollTo({
-                top: 0,
-                behavior: "smooth",
-              });
-            }}
-            //check if page is active
-            className={`${
-              page === item ? "bg-red-500" : "bg-gray-500"
-            } px-5 py-2 rounded-md hover:cursor-pointer hover:bg-red-500 hover:text-white`}
-          >
-            {item}
-          </p>
-        ))}
-      </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
--- components/Card.jsx
+++ components/Card.jsx
@@ -7,38 +7,35 @@ import { AiFillLike } from "react-icons/ai";
 import { useRouter } from "next/navigation";
 import ListsButton from "./ListsButton";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 function MovieCard({ movie, type }) {
   const router = useRouter();
   const { data: session } = useSession();
 
-  const handleOnClick = () => {
-    if (type === "movie") router.push(`/movies/${movie.id}`);
-    else router.push(`/tv/${movie.id}`);
-  };
-
   console.log(movie);
 
   return (
     <div className=" text-white rounded-md w-fit ">
-      <motion.div
-        onClick={() => handleOnClick()}
-        whileHover={{ scale: 1.05 }}
-        className="hover:cursor-pointer"
-      >
-        <Image
-          src={
-            movie.poster_path
-              ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-              : "/assets/not_found.jpg"
-          }
-          width={300}
-          height={300}
-          sizes="100vw"
-          alt="movie poster"
-          className="rounded-md"
-        />
-      </motion.div>
+      <Link href={type === "movie" ? `/movies/${movie.id}` : `/tv/${movie.id}`}>
+        <motion.div
+          whileHover={{ scale: 1.05 }}
+          className="hover:cursor-pointer"
+        >
+          <Image
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : "/assets/not_found.jpg"
+            }
+            width={300}
+            height={300}
+            sizes="100vw"
+            alt="movie poster"
+            className="rounded-md"
+          />
+        </motion.div>
+      </Link>
       <div className="mt-5">
         <div className="flex items-center justify-between">
           <h1 className="text-lg truncate w-48 font-bold text-left">
