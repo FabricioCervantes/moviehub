d5983e4fa49bc20d5098d9010d1ad43233ae7f6d
Fabricio <marcofabri6@gmail.com>
2023-07-28 19:36:55 -0600

    feat: add loading animation to movies page

--- app/movies/page.jsx
+++ app/movies/page.jsx
@@ -16,6 +16,7 @@ const Movies = () => {
   const [selectedGenre, setSelectedGenre] = useState(28);
   const [page, setPage] = useState(1);
   const [sort, setSort] = useState("popularity.desc");
+  const [loading, setLoading] = useState(true); // Agrega este estado para controlar la carga
 
   const { data: session } = useSession();
 
@@ -41,6 +42,7 @@ const Movies = () => {
   };
 
   const fetchMovies = async () => {
+    setLoading(true); // Mostrar el mensaje de espera
     const test = await fetchGenre();
     const upcomingMovies = await fetchUpcomingMovies();
 
@@ -75,6 +77,7 @@ const Movies = () => {
       setMovies(upcomingMovies.results);
       setGenre(test.genres);
     }
+    setLoading(false); // Ocultar el mensaje de espera
   };
 
   const handlePage = (item) => {
@@ -117,51 +120,72 @@ const Movies = () => {
 
   return (
     <>
-      <div className="flex text-white px-5 justify-end gap-5">
-        <Select onValueChange={(e) => handleGenre(e)}>
-          <SelectTrigger className="w-[180px] border-2">
-            <SelectValue placeholder="Genre" />
-          </SelectTrigger>
-          <SelectContent>
-            {genre.map((item) => (
-              <SelectItem key={item.id} value={item.id}>
-                {item.name}
-              </SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-        <Select onValueChange={(e) => handleSort(e)}>
-          <SelectTrigger className="w-[180px] border-2">
-            <SelectValue placeholder="Sort by" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="popularity.desc">Trending</SelectItem>
-            <SelectItem value="vote_count.desc">Popular</SelectItem>
-            <SelectItem value="revenue.desc">Box Office</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-      <DisplayMedia media={movies} type="movie" />
-      <div className="flex justify-end gap-5 p-2 mt-5">
-        {pag.map((item) => (
-          <p
-            key={item}
-            onClick={() => {
-              handlePage(item);
-              window.scrollTo({
-                top: 0,
-                behavior: "smooth",
-              });
-            }}
-            //check if page is active
-            className={`${
-              page === item ? "bg-red-500" : "bg-gray-500"
-            } px-5 py-2 rounded-md hover:cursor-pointer hover:bg-red-500 hover:text-white`}
+      {loading ? (
+        <div class="w-full h-screen bg text-white grid grid-cols-1 grid-rows-1 place-items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            fill="currentColor"
+            class="bi bi-arrow-repeat animate-spin"
+            viewBox="0 0 16 16"
           >
-            {item}
-          </p>
-        ))}
-      </div>
+            <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z" />
+            <path
+              fill-rule="evenodd"
+              d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"
+            />
+          </svg>
+        </div>
+      ) : (
+        <>
+          <div className="flex text-white px-5 justify-end gap-5">
+            <Select onValueChange={(e) => handleGenre(e)}>
+              <SelectTrigger className="w-[180px] border-2">
+                <SelectValue placeholder="Genre" />
+              </SelectTrigger>
+              <SelectContent>
+                {genre.map((item) => (
+                  <SelectItem key={item.id} value={item.id}>
+                    {item.name}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <Select onValueChange={(e) => handleSort(e)}>
+              <SelectTrigger className="w-[180px] border-2">
+                <SelectValue placeholder="Sort by" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="popularity.desc">Trending</SelectItem>
+                <SelectItem value="vote_count.desc">Popular</SelectItem>
+                <SelectItem value="revenue.desc">Box Office</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+          <DisplayMedia media={movies} type="movie" />
+          <div className="flex justify-end gap-5 p-2 mt-5">
+            {pag.map((item) => (
+              <p
+                key={item}
+                onClick={() => {
+                  handlePage(item);
+                  window.scrollTo({
+                    top: 0,
+                    behavior: "smooth",
+                  });
+                }}
+                //check if page is active
+                className={`${
+                  page === item ? "bg-red-500" : "bg-gray-500"
+                } px-5 py-2 rounded-md hover:cursor-pointer hover:bg-red-500 hover:text-white`}
+              >
+                {item}
+              </p>
+            ))}
+          </div>
+        </>
+      )}
     </>
   );
 };
